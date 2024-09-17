ca060b62cb9b7d16afe8a69f646db89d74445d11
Ilya <superbqost@gmail.com>
2024-09-17 23:38:32 +0500

    тернанрные операторы

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-const type = 'video';
+const role = 'User'
 
-if (type === 'music') {
-    console.log('Музыка');
-} else if (type === 'video') {
-    console.log('это видео бро...');
-} else {
-    console.log('Это не музыка бро!');
-}
-
-switch (type){
-    case 'music':
-        console.log('dolbit normalno');
-        break;
-    case 'video':
-        console.log('pokazivaet normalno');
-        break;
-    default:
-        console.log('ne slishno i ne vidno bro');
-}
-
-
+let text = role == 'Admin' ? 'Admin' : role == 'User' ? role : 'Not user'
+console.log(text)
