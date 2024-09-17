0b55f646643a9310778bc489ea4a106bf1f73029
Ilya <superbqost@gmail.com>
2024-09-17 22:36:39 +0500

    Расчет сложного процента

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const age = 18;
-console.log(`Мне ${age} лет`)
+let depositUSD = 12000
+let perc = 7
+let timeMonth = 24
+
+const itog = depositUSD * (1 + (perc/100)/12) ** timeMonth
+
+if (itog > 13500) {
+    console.log('Покупка доступна')
+} else     console.log('Покупка недоступна')
+
+
