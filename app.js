6f9d65a673ce21a2f894e5b17bd864e0e96f3ee4
Ilya <superbqost@gmail.com>
2024-09-17 23:56:16 +0500

    капча 2.0

--- app.js
+++ app.js
@@ -2,14 +2,8 @@ const answer = prompt('КАПЧА! \nСколько будет 7 + или - 15?'
 
 switch (answer) {
     case '8':
-        console.log('Успех!');
-        break;
     case '22':
-        console.log('Успех!');
-        break;
     case 'я не робот':
-        console.log('Успех!');
-        break;
     case 'Я не робот':
         console.log('Успех!');
         break;
