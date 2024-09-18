0de74f084e44c810f88525b3849526d6b22628dc
Ilya <superbqost@gmail.com>
2024-09-19 00:29:54 +0500

    Упражнение - проверка прав

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const answer = prompt('КАПЧА! \nСколько будет 7 + или - 15?')
+/*
+Пользователь хочет приобрести игру в магазине
+Он может это сделать отлько если:
+- Его баланс больше 1000 (balance) или число бонусов больше 100 (bonusBalance)
+- Он не забанен (¿sBanned)
+- Игра не кулена (isExist)
+- Игра в продаже (isSelling)
+Напишите условие для покупки и выведите в консоль результат
+*/
 
-switch (answer) {
-    case '8':
-    case '22':
-    case 'я не робот':
-    case 'Я не робот':
-        console.log('Успех!');
-        break;
-    default:
-        console.log('Прочь робот!');
-}
+const balance = 1000;
+const bonusBalance = 100;
+const isBanned = false;
+const isExist = true;
+const isSelling = true; 
 
+let user = 'Dragon777';
+
+const userBalance = 1000;
+const userBonusBalance = 50;
+
+const canBuy = (userBalance >= balance || userBonusBalance >= bonusBalance) && !isBanned && isExist && isSelling;
+
+if (canBuy) {
+console.log(`Пользователь ${user} может купить игру`)
+} else console.log(`Пользователь ${user} не может купить игру`)
 
